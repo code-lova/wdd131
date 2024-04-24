9c1bda058759f5a5fe34f6a3669a5d2c93e84de3
code-lova <ebinuga@hotmail.com>
2024-04-24 22:38:42 +0100

    home page assignment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,28 +1,10 @@
-'use strict';
-
 const currentYear = document.querySelector("#currentyear");
-
-const date = new Date().getFullYear();
-
-currentYear.innerHTML = date;
-
+currentYear.textContent = new Date().getFullYear();
 
 const lastMod = document.querySelector("#lastModified");
-
-function lastModified(mod){
-    mod = formatDate(new Date(document.lastModified));
-    return mod;
+function lastModified() {
+    const date = new Date(document.lastModified);
+    const formattedDate = date.toLocaleString('en-GB', { timeZone: 'UTC' }); // Adjust timeZone if needed
+    return formattedDate;
 }
-
-const formatDate = (date) => {
-    let day = ('0' + date.getDate()).slice(-2);
-    let month = ('0' + (date.getMonth() + 1)).slice(-2);
-    let year = date.getFullYear();
-    let hours = ('0' + date.getHours()).slice(-2);
-    let minutes = ('0' + date.getMinutes()).slice(-2);
-    let seconds = ('0' + date.getSeconds()).slice(-2);
-
-    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
-}
-
-lastMod.textContent = `Last Modification= ${lastModified()}`;
+lastMod.textContent = `Last Modification = ${lastModified()}`;
